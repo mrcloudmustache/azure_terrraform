169532e983d5901652d55aeb1945a210824ac1ac
James Thomas <mrcloudmustache@gmail.com>
2024-02-12 10:55:39 -0600

    delete vnet as test

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ resource "azurerm_resource_group" "main" {
   location = "northcentralus"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "customerA-vnet"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  address_space       = ["10.0.0.0/16"]
-}
+# resource "azurerm_virtual_network" "example" {
+#   name                = "customerA-vnet"
+#   location            = azurerm_resource_group.main.location
+#   resource_group_name = azurerm_resource_group.main.name
+#   address_space       = ["10.0.0.0/16"]
+# }
