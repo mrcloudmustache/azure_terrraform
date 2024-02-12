bfdbee3fe2389993e9f0aaa7527745f24e4731b3
James Thomas <mrcloudmustache@gmail.com>
2024-02-12 09:39:12 -0600

    added vnet testing change

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "main" {
   name     = "customerA"
   location = "northcentralus"
 }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "customerA-vnet"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+}
